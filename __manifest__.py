864f88725cdaa8c5bb7953d0a7647c814d8153a0
César Gtz <cesareomargtz@gmail.com>
2017-11-09 17:22:01 -0700

    migration to odoo v10

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'name': "news_app",
     'author': "Yecora",
     'version': '0.1',
-    'depends': ['base','market_price'],
+    'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
         'news.xml',
--- news.py
+++ news.py
@@ -5,10 +5,12 @@ from openerp import models, fields, api
 class news_app(models.Model):
     _name = 'news.app'
 
-    _defaults = {'name': lambda obj, cr, uid, context: obj.pool.get(
-        'ir.sequence').get(cr, uid, 'code_news'), }
+    # _defaults = {'name': lambda obj, cr, uid, context: obj.pool.get(
+    #     'ir.sequence').get(cr, uid, 'code_news'), }
 
-    name = fields.Char()
+    name = fields.Char( required=True, select=True, copy=False, default=lambda
+        self: self.env['ir.sequence'].next_by_code('code_news'), help="Unique number")
+
 
     title = fields.Char(string="Title", required=True)
     sub_title = fields.Char(string="Sub Title", required=True)
